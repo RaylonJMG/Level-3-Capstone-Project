42ba97ac87d220d5de126596fdc747f5650c7ed4
RaylonJMG <ryj2213@gmail.com>
2024-09-20 08:51:24 +0000

    🙎⏲ Checkpoint ./utility-functions.js:69253290/451 ./backup.html:69253290/590

--- utility-functions.js
+++ utility-functions.js
@@ -1,26 +1,9 @@
-function createColumnGroups( contentsArray, grouping) {
-  const myColumns = [];
-  let position = 0;   
-  do {
-    let columns = "";
-    for (let count = 0; count < grouping ; count++) {
-      const content = contentsArray[position];
-      if ( content !== undefined) {
-        columns += createColumn( content );  
-      }
-      position++;
-    }
-    myColumns.push( columns );
-  } while (position < contentsArray.length);  
-  return myColumns;
-}
-
 //CREATES COLUMNS FROM ARRAYS
 function createColumns(contentsArray){
   let columnString = "";
   for (let position=0; position < contentsArray.length; position++) {
     const content = contentsArray[position];
-    columnString += createColumn( content );   
+    columnString += createColumns( content );   
   } 
   return columnString;
 }
