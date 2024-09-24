3b361212f52fee1ee827d9d9dd446840db2c66b2
RaylonJMG <ryj2213@gmail.com>
2024-09-24 03:01:28 +0000

    🌌🍕 Checkpoint ./utility-functions.js:69253290/969 ./contact.html:69253290/241

--- utility-functions.js
+++ utility-functions.js
@@ -14,44 +14,6 @@ const cardString =  `<div class="card">
   return cardString;
 } 
 
-
-
-//CREATES COLUMNS FROM ARRAYS
-function createColumns(contentsArray){
-  let columnString = "";
-  for (let position=0; position < contentsArray.length; position++) {
-    const content = contentsArray[position];
-    columnString += createColumns( content );   
-  } 
-  return columnString;
-}
-
- //CREATES ROWS FROM AN ARRAY OF COLUMNS
-function createRows( columnsArray) {
-  let myRows = "";
-  for (let position = 0; position < columnsArray.length; position++) {
-    const columns = columnsArray[position];
-    myRows += createRows( columns );
-  }
-  return myRows;
-}
-
-function createColumn(contentString) {
-  const myColumnString = `<div class="col">${contentString}</div>`;
-  return myColumnString;
-}
-
-function createRow(columnString) {
-  const rowString = `<div class="row">
-      ${columnString}</div>`;
-  return rowString;
-}
-function createContainer(rowString){
-  const containerString = `<div class="container">
-    ${rowString}
-  </div>`;
-  return containerString;
-}
 function display(message){
     document.write(message);
   }
