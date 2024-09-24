ca1a0710bffae3c8c50266f74c7a40c48cdbd5c1
RaylonJMG <ryj2213@gmail.com>
2024-09-24 05:17:16 +0000

    🚓👑 Checkpoint ./utility-functions.js:69253290/98

--- utility-functions.js
+++ utility-functions.js
@@ -17,3 +17,7 @@ const cardString =  `<div class="card">
 function display(message){
     document.write(message);
   }
+
+function output(message) {
+  window.outputTag.innerHTML = window.outputTag.innerHTML + message;
+}
