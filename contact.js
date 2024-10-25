04ffba0dc678413b0efe5fde41c7e833d904b54d
RaylonJMG <ryj2213@gmail.com>
2024-10-25 00:54:09 -0500

    added contact form processing

--- contact.js
+++ contact.js
@@ -2,8 +2,6 @@ function myProcessForm(event) {
     debugger;
     event.preventDefault(); //prevents the page from refreshing
     const form = event.target;
-    const firstnameInput = form[1];
-    const lastnameInput = form[2];
-    const firstname = firstnameInput.value;
-    output(`Your name is ${firstname}.<br>`);
+    const phoneNumber = phoneNumber.value;
+    output(`Your phone number is ${phoneNumber}.<br>`);
 }
