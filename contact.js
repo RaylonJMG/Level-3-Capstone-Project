acf08a6bce131755401e4e043cfc71dfe96eed20
RaylonJMG <ryj2213@gmail.com>
2024-10-26 13:12:04 -0500

    corrected contact form to make it functional

--- contact.js
+++ contact.js
@@ -2,7 +2,11 @@ function processForm(event) {
     debugger;
     event.preventDefault();
     const form = event.target;
-    const input = form;
-    const value = input.value;
-    output("Your appointment has been scheduled!");
+    const phoneNumberInput = form[3];
+    const firstNameInput = form[0];
+    const calendarInput = form[2];
+    const phoneNumber = phoneNumberInput.value;
+    const firstName = firstNameInput.value;
+    const calendar = calendarInput.value;
+    output(`Thank you, ${firstName}. <br> Your phone appointment is scheduled for ${calendar}.<br> Expect a call at ${phoneNumber}.`);
 }
