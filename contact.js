f972e8986252b68f05202dc51f0b363cdbdfc3f1
RaylonJMG <ryj2213@gmail.com>
2024-11-18 14:35:38 -0600

    added server simulation

--- contact.js
+++ contact.js
@@ -9,7 +9,7 @@ function processForm(event) {
     const phoneNumber = phoneNumberInput.value;
     const firstName = firstNameInput.value;
     const calendar = calendarInput.value;
-    output(`Thank you, ${firstName}. <br> Your phone appointment is scheduled for ${calendar}.<br> You can expect a call at ${phoneNumber}. Have a great day.`);
+    output(`Thank you, ${firstName}. <br> Your phone appointment is scheduled for ${calendar}.<br> You can expect a call at ${phoneNumber}. Have a great rest of your day.`);
 
 }
 
@@ -22,18 +22,45 @@ function asynchronous() {
     setTimeout(5000);
 }
 //CONTACT FORM PROCESSING
-function submitForm(event) {
+function handleSubmit(event) {
     const inputs = event.target;
     const emailInput = inputs[1];
     const email = emailInput.value;
     document.write("Submitting form for <br>" + email + "...");
     setTimeout(serverResponse, 3000);
+    promise.then(parseResponse);
 }
 //CONTACT FORM PROCESSING
 function serverResponse() {
     display("Thanks! We have received your message!");
 }
+
+function parseResponse(resolveValue){
+    debugger;
+    const response = JSON.parse(resolveValue);
+    const message = response.message;
+    display(resolveValue);
+}
+
 //RATING FORM PROCESSING
-function ratingForm() {
-    display("Thanks for your rating. See you soon!");
+function handleClick() {
+    setTimeout(serverResponse2, 5000);
+    
+    function serverResponse2() {
+        display("Thanks! We have received your message!");
+}
+}
+//SERVER SIMULATION
+function getServerResponse(resolve){
+    setTimeout(activateResolve, 5000);
+
+    function activateResolve(){
+        debugger;
+        //SIMULATE A SERVER BY SENDING A STRINGIFIED OBJECT
+        const response = { 
+            message: "Successfully processed.Thanks for your rating. See you soon!"
+        };
+    const resolveValue = JSON.stringify(response);
+    resolve(resolveValue);
+    }
 }
