a426ba7da0714e11e7b12c3918ee3cb6005422a3
RaylonJMG <ryj2213@gmail.com>
2024-11-22 00:47:41 -0600

    corrected RestAPI string error

--- index.js
+++ index.js
@@ -1,52 +1,23 @@
 //FETCH FUNCTION sends request to API URL and returns a promise
 function handleClickAdvice(){
     debugger;
-    const promise = fetch("https://api.adviceslip.com/daily_adviceslip.rss");
-    promise.then(extractResponse);
+    const promise = fetch("	https://api.adviceslip.com/advice");
+    promise.then(extractAdvice);
 }
 
 //Resolve value is a special object created by the fetch function
 //Text Method extracts stringified object
-function extractResponse(resolveValue){
-    const promise = resolveValue.text();
+function extractAdvice(resolveValue){
+    const promise = resolveValue.text(); 
     promise.then(viewAdviceResponse);
 } 
 
 //JSON.parse function to convert special object to a stringified object
-//parse method generates an object from the string
-//stringify method generates a string from an object
+//JSON.parse method generates an object from the string
+//JSON.stringify method generates a string from an object
 function viewAdviceResponse(resolveValue){
     debugger;
-    const advice = JSON.stringify(resolveValue);
+    const result = JSON.parse(resolveValue);
+    const advice = result.slip.advice;
     output(advice);
-}
-
-function getAdvice() { 
-    const promise = fetch("https://api.adviceslip.com/advice");
-}
-/*******************************************
-//FETCH FUNCTION
-function handleClick(event){
-    debugger;
-    const promise = fetch("https://api.adviceslip.com/advice");
-    promise.then(parseResponse);
-}
-function handleAdvice(){
-    debugger;
-    const baseURL = "https://api.adviceslip.com";
-    const endPoint = "/advice";
-    const URL = (baseURL + endPoint);
-    const promise = fetch(URL);
-    promise.then(extractResponse);
-}
-//<input onclick="handleAdvice()"></input>
-
-function extractResponse(resolveValue){
-    const promise = resolveValue.text();
-    promise.then(parseAdviceResponse);
-}
-function parseAdviceResponse(resolveValue){
-    debugger;
-    const response = JSON.parse();
-}
-*******************************************/
+};
