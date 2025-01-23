0a85e85556175a8f15ae309cea911716a7358790
RaylonJMG <ryj2213@gmail.com>
2025-01-23 02:23:37 -0600

    moved files into React folder structure and updated output file

--- modules/parseResponse.js
+++ modules/parseResponse.js
@@ -1,10 +1,10 @@
-import { outputModule } from "../utils/outputModule.js";
+import { outputModule } from "../src/utils/outputModule.js";
 
 //PARSE :ANALYZE/CONVERT DATA TO USABLE FORMAT
 export function parseResponse(resolveValue) {
-        debugger;
-        //JSON.PARSE: CONVERTS STRING OBJECT INTO ACTUAL OBJECT
-        const response = JSON.parse(resolveValue);
-        const message = response.message;
-        outputModule(resolveValue);
-    }
+	debugger;
+	//JSON.PARSE: CONVERTS STRING OBJECT INTO ACTUAL OBJECT
+	const response = JSON.parse(resolveValue);
+	const message = response.message;
+	outputModule(resolveValue);
+}
--- utils/output.js
+++
@@ -1,4 +0,0 @@
-export function output(message = "message", outputTag = "outputTag", shouldAppend = true) {
-    if (shouldAppend) window[outputTag].innerHTML += message;
-    else window[outputTag].innerHTML += message;
-}
