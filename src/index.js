9e7e5e061018f31bf039fbf84bb3edfe303bf65a
RaylonJMG <ryj2213@gmail.com>
2025-01-23 03:39:13 -0600

    BrowserRouter setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 const root = createRoot(window.bodyTag);
-root.render(
-	/*#
-   _ PURE_
-   */
-	React.createElement("h1", null, "React is fabtastic!")
-);
+root.render(/*#__PURE__*/React.createElement(BrowserRouter, null, /*#__PURE__*/React.createElement(Routes, null, /*#__PURE__*/React.createElement(Route, {
+  path: "/",
+  element: /*#__PURE__*/React.createElement(Home, null)
+}), /*#__PURE__*/React.createElement(Route, {
+  path: "/about",
+  element: /*#__PURE__*/React.createElement(About, null)
+}), /*#__PURE__*/React.createElement(Route, {
+  path: "/contact",
+  element: /*#__PURE__*/React.createElement(Contact, null)
+}), /*#__PURE__*/React.createElement(Route, {
+  path: "/menus",
+  element: /*#__PURE__*/React.createElement(Menus, null)
+}), /*#__PURE__*/React.createElement(Route, {
+  path: "/photos",
+  element: /*#__PURE__*/React.createElement(Photos, null)
+}))));
--- src/index.original.js
+++ src/index.original.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const root = createRoot(window.bodyTag);
 root.render(
-	/*#
-    _ PURE_
-    */ React.createElement("h1", null, "React is amazing!")
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/menus" element={<Menus />} />
+			<Route path="/photos" element={<Photos />} />
+		</Routes>
+	</BrowserRouter>
 );
