cdfdf14536a51db1569d5d93abe4878e2bda89a5
RaylonJMG <ryj2213@gmail.com>
2025-01-25 02:42:21 -0600

    created Footer

---
+++ src/utils/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<main>
+			<footer style={{ textAlign: "center", fontSize: 6 }}>
+				<br />
+				<br /> Good Food Hut ❃12345 Big Bird Ave, Sesame Street, LA 12345 ❃
+				337-555-4321❃
+				<br />
+				Website Created by RaylonJMG 2024
+			</footer>
+		</main>
+	);
+}
