b9d4d72dacb197fff2f6484a3f16bda45b0e299b
RaylonJMG <ryj2213@gmail.com>
2025-01-25 01:52:56 -0600

    corrected index.html and home page

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,33 +1,17 @@
 import React from "react";
+import { GrowSpinner } from "../utils/GrowSpinner.js";
+import { NavBar } from "../utils/NavBar.js";
 
 export function Home() {
 	return (
-		<body
-			id="bodyTag"
-			class="m-5 p-4 border border-5 border-danger border-ridge">
+		<>
 			<header>
 				<img
 					width="100%"
 					alt="fried seafood platter"
 					src="https://cdn.glitch.global/cbca519c-2e2c-44e5-a058-374e1c8be1a2/Screenshot%202024-08-03%20021209.jpg?v=1722669203847"
 				/>
-				<nav class="nav nav-fill nav-pills">
-					<a class="nav-link active" aria-current="page" href="index.html">
-						HOME
-					</a>
-					<a class="nav-link" href="about.html">
-						ABOUT
-					</a>
-					<a class="nav-link" href="menus.html">
-						MENU
-					</a>
-					<a class="nav-link" href="photos.html">
-						PHOTOS
-					</a>
-					<a class="nav-link" href="contact.html">
-						CONTACT
-					</a>
-				</nav>
+				<NavBar />
 			</header>
 			<main>
 				<article class="container">
@@ -37,7 +21,7 @@ export function Home() {
 					</div>
 					<h3 class="p-1 text-center">Delighting Tastebuds since 1988</h3>
 					<br />
-					<div class="spinner-grow text-primary"></div>
+					<GrowSpinner />
 					<p class="text-start fs-3">
 						Visit our Sesame Street Location and try all that the Good Food Hut
 						has to offer. We have been sharing our Good Food Philosophy for over
@@ -49,7 +33,7 @@ export function Home() {
 						width="100%"
 					/>
 					<hr />
-					<p class="text-start fs-3">
+					<p>
 						So whether it's for some quality time with the family,
 						"chillaxation" time with your friends, or decompression time with
 						the colleagues...we have created a space that honors a relaxed and
@@ -79,6 +63,6 @@ export function Home() {
 					Website Created by RaylonJMG 2024
 				</footer>
 			</h6>
-		</body>
+		</>
 	);
 }
--- src/index.babel.js
+++ src/index.babel.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-const root = createRoot(window.bodyTag);
-root.render(/*#__PURE__*/React.createElement(BrowserRouter, null, /*#__PURE__*/React.createElement(Routes, null, /*#__PURE__*/React.createElement(Route, {
+"use strict";
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { "default": obj };
+}
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDomClient = require("react-dom/client");
+
+var _reactRouter = require("react-router");
+
+var root = (0, _reactDomClient.createRoot)(window.bodyTag);
+root.render( /*#__PURE__*/_react2["default"].createElement(_reactRouter.BrowserRouter, null, /*#__PURE__*/_react2["default"].createElement(_reactRouter.Routes, null, /*#__PURE__*/_react2["default"].createElement(_reactRouter.Route, {
   path: "/",
-  element: /*#__PURE__*/React.createElement(Home, null)
-}), /*#__PURE__*/React.createElement(Route, {
+  element: /*#__PURE__*/_react2["default"].createElement(Home, null)
+}), /*#__PURE__*/_react2["default"].createElement(_reactRouter.Route, {
   path: "/contact",
-  element: /*#__PURE__*/React.createElement(Contact, null)
-}), /*#__PURE__*/React.createElement(Route, {
+  element: /*#__PURE__*/_react2["default"].createElement(Contact, null)
+}), /*#__PURE__*/_react2["default"].createElement(_reactRouter.Route, {
   path: "/about",
-  element: /*#__PURE__*/React.createElement(About, null)
+  element: /*#__PURE__*/_react2["default"].createElement(About, null)
 }))));
