59c0449c079a152c0b0479252340be51433e49b2
RaylonJMG <ryj2213@gmail.com>
2025-02-14 02:03:28 -0600

    added Carousel component

---
+++ src/Views/Carousel.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+export function Carousel() {
+	return (
+		<div
+			id="myPictures"
+			className="carousel slide">
+			<div className="carousel-indicators">
+				<button
+					type="button"
+					data-bs-target="#myPictures
+                "
+					data-bs-slide-to="0"
+					className="active"
+					aria-current="true"
+					aria-label="Slide 1"></button>
+				<button
+					type="button"
+					data-bs-target="#myPictures
+                "
+					data-bs-slide-to="1"
+					aria-label="Slide 2"></button>
+				<button
+					type="button"
+					data-bs-target="#myPictures
+                "
+					data-bs-slide-to="2"
+					aria-label="Slide 3"></button>
+			</div>
+			<div className="carousel-inner">
+				<div className="carousel-item active">
+					<img
+						src={src}
+						className="d-block w-100"
+						alt="CheeseBurger"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src={src}
+						className="d-block w-100"
+						alt="Gumbo"
+					/>
+				</div>
+				<div className="carousel-item">
+					<img
+						src={src}
+						className="d-block w-100"
+						alt="ChefsKissSalad"
+					/>
+				</div>
+			</div>
+			<button
+				className="carousel-control-prev"
+				type="button"
+				data-bs-target="#myPictures
+            "
+				data-bs-slide="prev">
+				<span
+					className="carousel-control-prev-icon"
+					aria-hidden="true"></span>
+				<span className="visually-hidden">Previous</span>
+			</button>
+			<button
+				className="carousel-control-next"
+				type="button"
+				data-bs-target="#myPictures
+            "
+				data-bs-slide="next">
+				<span
+					className="carousel-control-next-icon"
+					aria-hidden="true"></span>
+				<span className="visually-hidden">Next</span>
+			</button>
+		</div>
+	);
+}
