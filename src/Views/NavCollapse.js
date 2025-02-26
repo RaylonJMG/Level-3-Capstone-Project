73b96f0c279cc9673df292e59de31aa09ea8ced2
RaylonJMG <ryj2213@gmail.com>
2025-02-26 00:36:06 -0600

    saved github path to navbar

--- src/Views/NavCollapse.js
+++ src/Views/NavCollapse.js
@@ -5,7 +5,7 @@ export function NavCollapse() {
 	//debugger;
 	const domain = window.location.hostname;
 	let rootPath = "";
-	if (domain === "RaylonJMG.github.io") rootPath = "good-food-hut";
+	if (domain === "raylonjmg.github.io") rootPath = "good-food-hut";
 
 	return (
 		<nav className="nav-pills navbar navbar-expand-sm bg-body-tertiary">
