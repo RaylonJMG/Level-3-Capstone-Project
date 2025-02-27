97ea05c81d5df912372e2d4dc65ac6b5c0847b28
RaylonJMG <ryj2213@gmail.com>
2025-02-26 22:44:14 -0600

    minor change to carousel

--- dist/main.js
+++ dist/main.js
@@ -2243,7 +2243,7 @@ __webpack_require__.r(__webpack_exports__);
 
 function Carousel() {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-    id: " myPictures",
+    id: "myPictures",
     className: "carousel slide"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
     className: "carousel-indicators"
