f4abd92dbea34dbc75d16a6ea6f223f4b022a26e
RaylonJMG <ryj2213@gmail.com>
2025-03-05 19:57:53 -0600

    added jest packages, updated babel preset file, and created a test file

---
+++ src/modules/authenticationAWS.test.js
@@ -0,0 +1,98 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("The authenticationDB function", () => {
+	it("authenticates when email and password are correct", async () => {
+		//ARRANGE
+		const email = "abc@logins.com";
+		const password = "aaa";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(true);
+	});
+	it("does not authenticate when email and password are incorrect", async () => {
+		//ARRANGE
+		const email = "acb@logins.com";
+		const password = "acb";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is correct, but the password is incorrect", async () => {
+		//ARRANGE
+		const email = "abc@logins.com";
+		const password = "acb";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is incorrect, but the password is correct", async () => {
+		//ARRANGE
+		const email = "acb@logins.com";
+		const password = "aaa";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is empty, but the password is correct", async () => {
+		//ARRANGE
+		const email = "";
+		const password = "aaa";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is correct, but the password is empty", async () => {
+		//ARRANGE
+		const email = "abc@logins.com";
+		const password = "";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is not in the list", async () => {
+		//ARRANGE
+		const email = "lmnop@logins.com";
+		const password = "lmnop";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is not in the list, but the password is in the list", async () => {
+		//ARRANGE
+		const email = "lmnop@logins.com";
+		const password = "aaa";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is in the list, but the password is empty", async () => {
+		//ARRANGE
+		const email = "abc@logins.com";
+		const password = "";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+	it("does not authenticate when email is in the list, but the password is a Number", async () => {
+		//ARRANGE
+		const email = "abc@logins.com";
+		const password = "12345678";
+		//ACT
+		const isAuthenticated = await authenticationDB(email, password);
+		//ASSERT
+		expect(isAuthenticated).toBe(false);
+	});
+});
+
+//still need to import authentication file from DynamoDB branch
+
+//need to run the tests npx jest to make sure they work properly
